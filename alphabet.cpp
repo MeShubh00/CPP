9ab9063ce30b3606772e6b78f6b187a0198b4c46
MeShubh00 <shubhamshedage03@gmail.com>
2026-01-31 16:24:04 +0530

    alphabet

---
+++ alphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n=25;
+       char ch = 'A';
+    
+    for(int i=0;i<n;i++)
+    {
+        cout <<ch<<endl;
+        ch =ch + 1;
+    }
+
+    return 0;
+}
